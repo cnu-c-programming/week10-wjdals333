9ecc80465bfcfbf1eb6702ddee2d26a24aa8cbc4
Kim Jeongmin <131476493+wjdals333@users.noreply.github.com>
2026-05-09 01:04:17 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -17,11 +17,12 @@ int main() {
     s.protocol = 0;
     sprintf(s.data, "hello world");
 
-    printf("version: %u, size %zu\n", s.version, sizeof(s.version));
-    printf("ihl: %u, sizee: %zu\n", s.ihl, sizeof(s.ihl));
+    printf("version: %u, size: %zu\n", s.version, sizeof(s.version));
+    printf("ihl: %u, size: %zu\n", s.ihl, sizeof(s.ihl));
     printf("ttl: %llu, size: %zu\n", s.ttl, sizeof(s.ttl));
     printf("protocol: %llu, size: %zu\n", s.protocol, sizeof(s.protocol));
     printf("data: %s, size: %zu\n", s.data, sizeof(s.data));
     
     return 0;
 }
+
